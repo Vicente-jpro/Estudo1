b7118eb5507a66ec670b38e362787a98325a950e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-22 13:56:15 +0100

    Cller home added

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
 public class HomeController {
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public String home() {
+		return "Hello estudo 1 aplication";
+	}
 
 }
