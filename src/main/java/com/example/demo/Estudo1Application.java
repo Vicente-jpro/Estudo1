ae7a5b6014b930312d4b0b394aefa5fcb33a2a21
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-06-22 10:47:24 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Estudo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estudo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estudo1Application.class, args);
+	}
+
+}
